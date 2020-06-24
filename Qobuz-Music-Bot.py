d2ae7c3381b033f8f876ab03324ddaae28e82aa6
thisisawesome1994 <58063460+thisisawesome1994@users.noreply.github.com>
2020-06-24 15:57:20 +0200

    Add files via upload

--- Qobuz-Music-Bot.py
+++ Qobuz-Music-Bot.py
@@ -11,41 +11,41 @@ view_time = float(input('enter time for each view: '))
 
 # put video link in ''
 for i in range(views):
-        f = open('links.txt', 'r', encoding='utf-8')
-        for line in f:
-            url = line
-        f = open('proxies.txt', 'r', encoding='utf-8')
-        for line in f:
-            proxy = line
-        f = open('username.txt', 'r', encoding='utf-8')
-        for line in f:
-            username = line
-        f = open('password.txt', 'r', encoding='utf-8')
-        for line in f:
-            password = line
-        f = open('useragents.txt', 'r', encoding='utf-8')
-        for line in f:
-            useragent = line
-            opts1 = Options()
-            opts1.add_argument('--user-agent=%s'% useragent)
-            #opts1.add_argument('--mute-audio')
-            opts1.add_argument('--incognito')
-            opts1.add_argument('--proxy-server=%s'% proxy)
-            #opts1.add_argument('--headless')
-            opts1.add_argument('--start-maximized')
-            browser1 = webdriver.Chrome(options=opts1)
-            browser1.execute_script("window.location.replace(arguments[0])", url)
-            time.sleep (3)
-            login = browser1.find_element_by_xpath(""" //*[@id="root"]/div/div/div[3]/div/div[2]/div/div[3]/form/div[1]/div/fieldset/input """).send_keys(username)
-            password = browser1.find_element_by_xpath(""" //*[@id="root"]/div/div/div[3]/div/div[2]/div/div[3]/form/div[2]/div/fieldset/input """).send_keys(password)
-            submit = browser1.find_element_by_xpath(""" //*[@id="root"]/div/div/div[3]/div/div[2]/div/div[3]/form/p[1]/button """)
-            submit.click()
-            time.sleep(5)
-            play_button = browser1.find_element_by_xpath(""" //*[@id="root"]/div/div/div[3]/div/div[2]/section/div/div/div/div/div/ul/li[2]/div/span[1]/a/span """)
-            play_button.click()
-            time.sleep(view_time)
-
-browser1.close()
-
-
+    f = open('links.txt', 'r', encoding='utf-8')
+    for line in f:
+        url = line
+    f = open('proxies.txt', 'r', encoding='utf-8')
+    for line in f:
+        proxy = line
+    f = open('username.txt', 'r', encoding='utf-8')
+    for line in f:
+        username = line
+    f = open('password.txt', 'r', encoding='utf-8')
+    for line in f:
+        password = line
+    f = open('useragents.txt', 'r', encoding='utf-8')
+    for line in f:
+        useragent = line
+    opts1 = Options()
+    opts1.add_argument('--user-agent=%s'% useragent)
+    #opts1.add_argument('--mute-audio')
+    opts1.add_argument('--incognito')
+    opts1.add_argument('--proxy-server=%s'% proxy)
+    #opts1.add_argument('--headless')
+    opts1.add_argument('--start-maximized')
+    browser1 = webdriver.Chrome(options=opts1)
+    browser1.execute_script("window.location.replace(arguments[0])", url)
+    time.sleep(10)
+    login = browser1.find_element_by_xpath(""" //*[@id="root"]/div/div/div[3]/div/div[2]/div/div[3]/form/div[1]/div/fieldset/input """).send_keys(username)
+    time.sleep(2)
+    password = browser1.find_element_by_xpath(""" //*[@id="root"]/div/div/div[3]/div/div[2]/div/div[3]/form/div[2]/div/fieldset/input """).send_keys(password)
+    time.sleep(2)
+    submit = browser1.find_element_by_xpath(""" //*[@id="root"]/div/div/div[3]/div/div[2]/div/div[3]/form/p[1]/button """)
+    submit.click()
+    time.sleep(15)
+    play_button = browser1.find_element_by_xpath(""" //*[@id="root"]/div/div/div[3]/div/div[2]/section/div/div/div/div/div/ul/li[2]/div/span[1]/a/span """)
+    time.sleep(2)
+    play_button.click()
+    time.sleep(view_time)
+    browser1.quit()
 # script by thisisawesome1994
