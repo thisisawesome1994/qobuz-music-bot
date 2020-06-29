4e4d93f7d9118ccc213f807b4e6690b4992476f3
thisisawesome1994 <58063460+thisisawesome1994@users.noreply.github.com>
2020-06-29 03:47:31 +0200

    Add files via upload

--- Qobuz-Music-Bot.py
+++ Qobuz-Music-Bot.py
@@ -14,8 +14,6 @@ import random
 proxy = random.choice(open('proxies.txt').readlines())
 useragent = random.choice(open('useragents.txt').readlines())
 url = random.choice(open('links.txt').readlines())
-username = open('username.txt', 'r', encoding='utf-8').readlines()
-password = open('password.txt', 'r', encoding='utf-8').readlines()
 
 
 opts1 = Options()
@@ -28,13 +26,18 @@ opts1.add_argument('--start-maximized')
 browser1 = webdriver.Chrome(options=opts1)
 browser1.execute_script("window.location.replace(arguments[0])", url)
 time.sleep(10)
-login = browser1.find_element_by_xpath(""" //*[@id="root"]/div/div/div[3]/div/div[2]/div/div[3]/form/div[1]/div/fieldset/input """).send_keys(username)
-time.sleep(2)
-password = browser1.find_element_by_xpath(""" //*[@id="root"]/div/div/div[3]/div/div[2]/div/div[3]/form/div[2]/div/fieldset/input """).send_keys(password)
-time.sleep(2)
-submit = browser1.find_element_by_xpath(""" //*[@id="root"]/div/div/div[3]/div/div[2]/div/div[3]/form/p[1]/button """)
-submit.click()
-time.sleep(15)
+with open('credentials.txt') as f:
+    credentials = [x.strip().split(':', 1) for x in f]
+#username = f.x.strip().split(':')[0]
+#password = f.x.strip().split(':')[1]
+for username, password in credentials:
+    browser1.find_element_by_xpath(""" //*[@id="root"]/div/div/div[3]/div/div[2]/div/div[3]/form/div[1]/div/fieldset/input """).send_keys(username)
+    time.sleep(2)
+    browser1.find_element_by_xpath(""" //*[@id="root"]/div/div/div[3]/div/div[2]/div/div[3]/form/div[2]/div/fieldset/input """).send_keys(password)
+    time.sleep(2)
+    submit = browser1.find_element_by_xpath(""" //*[@id="root"]/div/div/div[3]/div/div[2]/div/div[3]/form/p[1]/button """)
+    submit.click()
+    time.sleep(15)
 play_button = browser1.find_element_by_xpath(""" //*[@id="root"]/div/div/div[3]/div/div[2]/section/div/div/div/div/div/ul/li[2]/div/span[1]/a/span """)
 time.sleep(2)
 play_button.click()
